70e7def7cef7f46c20e51148826933e772bd39c8
dmon23 <deanmontilde@gmail.com>
2023-07-24 22:33:04 +1200

    added A* pathing

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -19,13 +19,6 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        // solution 1
-        // float x_pos = (player.transform.position[0] > transform.position[0] ? 1 : -1) * enemySpeed * Time.deltaTime;
-        // float y_pos = (player.transform.position[1] > transform.position[1] ? 1 : -1) * enemySpeed * Time.deltaTime;
-        // transform.position = transform.position + new Vector3 (x_pos, y_pos, 0);
-
-        //solution 2
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemySpeed * Time.deltaTime);
 
 
     }
@@ -33,13 +26,6 @@ public class Enemy : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log(other);
-        // health -= 5;
-        // Debug.Log(gameObject);
-
-        // if(health <= 5) 
-        // {
-
-        // }
     }
 
      private void OnTriggerEnter2D(Collider2D other) 
